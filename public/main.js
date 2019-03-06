83a9b9b996e555061f247ecb4500f536e4056cac
Nemanja Danev <nemanja.danev.93@gmail.com>
2019-03-06 09:18:01 -0500

    added google map in third section

---
+++ public/main.js
@@ -0,0 +1,21 @@
+
+
+function initMap() {
+
+    var lat = parseFloat('44.7866');
+    var lng = parseFloat('20.4489');
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        
+        center: {lat: lat, lng: lng},
+        zoom: 10
+    });
+
+    var marker = new google.maps.Marker({
+        map: map,
+        position: {lat: lat, lng: lng},
+        title: 'Aura Group'
+    });
+
+
+}
