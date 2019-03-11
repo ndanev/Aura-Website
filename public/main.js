8bd2bb822434404df0502342e9963cdb36368a7a
Nemanja Danev <nemanja.danev.93@gmail.com>
2019-03-11 05:44:34 -0400

    added jquery functionality for navbar links

--- public/main.js
+++ public/main.js
@@ -1,29 +1,47 @@
 
-// init google map
+$(document).ready(function () {
+
+    $(window).on('load', function() {
+
+        $('html, body').animate({scrollTop: 0});
+
+    });
+
+    $('ul.tabs a').on('click', function() {
+
+        $('ul.tabs a.active').removeClass('active');
+        $(this).addClass('active');
+
+    });
+
+    // functionality for scrolling down 
+    $('#arrow-btn').on('click', () => {
+        $('html, body').animate({ scrollTop: 810 });
+    });
+
+    $('.logo').on('click', () => {
+        $('html, body').animate({ scrollTop: 0 });
+    });
+
+});
+
+
+// google map
 function initMap() {
 
     var lat = parseFloat('44.7866');
     var lng = parseFloat('20.4489');
 
     var map = new google.maps.Map(document.getElementById('map'), {
-        
-        center: {lat: lat, lng: lng},
+
+        center: { lat: lat, lng: lng },
         zoom: 10
     });
 
     var marker = new google.maps.Marker({
         map: map,
-        position: {lat: lat, lng: lng},
+        position: { lat: lat, lng: lng },
         title: 'Aura Group'
     });
 
 }
-
-// functionality for scrilling down 
-$('#arrow-btn').on('click', () => {
-    $('html, body').animate({scrollTop: 810}, '200');
-});
-
-$('.logo').on('click', () => {
-    $('html, body').animate({scrollTop: 0});
-})
